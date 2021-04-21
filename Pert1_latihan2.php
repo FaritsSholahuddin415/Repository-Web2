9c283f8733595c66fd697c9e299caf2283028edd
Farits Sholahuddin <82789157+FaritsSholahuddin415@users.noreply.github.com>
2021-04-21 20:58:12 +0700

    Add files via upload
    pertemuan1 farits sholahuddin

---
+++ Pert1_latihan2.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>Variabel Global dan Local</title></head>
+<body><h1>Variabel Global dan Local</h1>
+<?php 
+$A = 123 ; // variable global
+function Test() { 
+global $A ; // variable local
+echo"Nilai A dalam fungsi = $A \n"; 
+} 
+Test(); 
+echo"Nilai A luar fungsi = $A \n"; 
+?>
+</body>
+</html>
